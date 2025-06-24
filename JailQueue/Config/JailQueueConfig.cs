d23233ebea43e8bdb3eecef659b7e5ad3b69b683
akel21 <a.3066@mail.ru>
2025-06-24 18:36:24 +0500

    add description to config

--- JailQueue/Config/JailQueueConfig.cs
+++ JailQueue/Config/JailQueueConfig.cs
@@ -5,6 +5,9 @@ namespace JailQueue.Config;
 
 public class JailQueueConfig : BasePluginConfig
 {
+    /// <summary>
+    /// Сколько нужно Т для одного КТ
+    /// </summary>
     [JsonPropertyName(nameof(Balance))]
     public int Balance { get; set; } = 4;
 }
